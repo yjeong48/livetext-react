5cb20cde43caa81e762efeba220d2df8d45091ee
yjeong48 <71805570+yjeong48@users.noreply.github.com>
2022-11-15 13:11:21 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React,{Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	state = {
+	// Initially, no file is selected
+	selectedFile: null,
+	target_lang: ""
+	};
+
+	// After selecting file from pop up
+	onFileChange = event => {
+	this.setState({ selectedFile: event.target.files[0] });	
+	};
+	
+	// After clicking the translate button)
+	onFileUpload = () => {
+	
+	const formData = new FormData();
+	formData.append(
+		"file",
+		this.state.selectedFile,
+		this.state.selectedFile.name
+	);
+	
+	// Details of the uploaded file
+	console.log(this.state.selectedFile);
+	
+	// Request made to the backend api
+	axios.post("http://20.42.63.116:5000", formData)
+	.then(response => this.setState({ translated_text: response.data}))
+	.catch(function (error) {
+		console.log(error)
+	})
+	}
+	
+	//Display translated text
+	translated = () => {
+		return (
+			<div>
+				<p>Translated text: </p>
+				<p>{this.state.translated_text}</p>
+			</div>
+		)
+	}
+	
+	render() {
+	return (
+		<div style={{textAlign:"center"}}>
+			<h1>
+			OCR Personal Project
+			</h1>
+			<h3>
+			Translate text from any image to Korean
+			</h3>
+			<div>
+				<input type="file" onChange={this.onFileChange} />
+				<button onClick={this.onFileUpload}>
+				Translate!
+				</button>
+			</div>
+		{this.translated()}
+		</div>
+	);
+	}
 }
 
 export default App;
+
+
